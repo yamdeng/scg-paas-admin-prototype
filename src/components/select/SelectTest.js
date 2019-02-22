11e1e1dfa8183407bf7dae1a82c9a481d9d8b32f
yamdeng <yamdeng@gmail.com>
2019-02-22 17:53:02 +0900

    react-select2 작업중

--- src/components/select/SelectTest.js
+++ src/components/select/SelectTest.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
 import Select from 'react-select';
+import AsyncSelect from 'react-select/lib/Async';
+import Api from '../../utils/Api';
 
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
@@ -16,12 +18,37 @@ const options = [
 class SelectTest extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { selectedOption: null };
+    this.state = {
+      selectedOption: null,
+      selectedOption2: null,
+      select2Data: []
+    };
     this.handleChange = this.handleChange.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
   }
 
   handleChange(selectedOption) {
     this.setState({ selectedOption });
+  }
+
+  handleChange2(selectedOption2) {
+    this.setState({ selectedOption2 });
+  }
+
+  onInputChange(inputValue, action) {
+    // debugger;
+    // Api.get('selectSearch', { params: { search: inputValue } }).then(result => {
+    //   this.setState({ select2Data: result.data.data });
+    // });
+    if (action === 'input-change') {
+      debugger;
+      Api.get('selectSearch', { params: { search: inputValue } }).then(
+        result => {
+          this.setState({ select2Data: result.data.data });
+        }
+      );
+    }
   }
 
   submit() {}
@@ -31,7 +58,7 @@ class SelectTest extends React.Component {
   }
 
   render() {
-    const { selectedOption } = this.state;
+    const { selectedOption, selectedOption2 } = this.state;
     return (
       <div class="container form-test">
         <div class="row">
@@ -43,8 +70,15 @@ class SelectTest extends React.Component {
               options={options}
             />
           </div>
-          <div class="col col-3">라벨2</div>
-          <div class="col col-9">값2</div>
+          <div class="col col-3">select2(ajax)</div>
+          <div class="col col-9">
+            {/* <AsyncSelect
+              isMulti
+              cacheOptions
+              defaultOptions
+              loadOptions={promiseOptions}
+            /> */}
+          </div>
           <div class="col col-3">라벨3</div>
           <div class="col col-9">값3</div>
           <div class="col col-3">라벨4</div>
